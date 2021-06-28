c894120237544c767fd35a540599ecdd2b5364a3
Ryan Jung <ryan.matthew.jung@gmail.com>
2021-06-28 16:13:14 -0400

    Update main.v

--- main.v
+++ main.v
@@ -1,38 +1,16 @@
 import parser
 import eval
 import os { input }
-fn f(arr []int) int {
-	mut sum := 0
-	for n in arr {
-		sum += n
-	}
-	return sum
-}
 
-
-
-
-fn main() {
-	// println(call_func(voidptr(add), 1, 2))
-	//println(parser.parse("(+ 1 1)"))
-
-	// println(eval.eval(parser.parse("1")))
-	// println(eval.eval(parser.parse("(+ 1 1)")))
-	// println(eval.eval(parser.parse("(+ 1 100)")))
-	// println(eval.eval(parser.parse("(add1 41)")))
-	// println(eval.eval(parser.parse("(lambda (x) x)")))
-
-	//println(eval.eval(parser.parse("((lambda (x) ((lambda (y) (+ x y)) 5)) 7)")))
-
+fn start_repl() {
 	env := eval.create_standard_environment()
 	for true {
 		raw_expr := input(" > ")
 		println(eval.eval_helper(parser.parse(raw_expr), env))
 	}
-
-	// println(eval.eval(parser.parse("#t")))
-	// println(eval.eval(parser.parse("#f")))
-	//println(eval.eval(parser.parse("(+ 1 1)")))
- 
 }
 
+
+fn main() {
+	start_repl()
+}
